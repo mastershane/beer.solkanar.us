735b908990cfe1985c545211fbfc8cbad39b0c52
mastershane <shanenotsean@hotmail.com>
2015-10-06 17:42:33 -0500

    Showing logged in message.  Fixing issue with the Use provider

--- app.js
+++ app.js
@@ -22,28 +22,41 @@ app.config(['$routeProvider',function($routeProvider) {
 			templateUrl: 'templates/login.html',
 			controller: 'loginController'
 		})
-        .when('/profile',{
-            templateUrl: 'templates/profile.html',
-            controller: 'profileController'
-        });
+		.when('/profile',{
+			templateUrl: 'templates/profile.html',
+			controller: 'profileController'
+		});
 }]);
 
 app.factory("Auth", ["$firebaseAuth",
   function ($firebaseAuth) {
-      var ref = new Firebase("https://beer-sol-kanar.firebaseio.com/");
-      return $firebaseAuth(ref);
+	  var ref = new Firebase("https://beer-sol-kanar.firebaseio.com/");
+	  return $firebaseAuth(ref);
   }
 ]);
 
-app.factory("User", ["Auth", "$firebase", function (Auth, $firebase) {
-    var authUser = Auth.$getAuth();
-    if (authUser) {
-        var ref = new Firebase("https://beer-sol-kanar.firebaseio.com/users/" + authUser.uid);
-        var sync = $firebase(ref);
-        return sync.$asObject();
-    }
-    return null;
+app.factory("UserProvider", ["Auth", "$firebase", function (Auth, $firebase) {
+
+	function getUser() {
+		var authUser = Auth.$getAuth();
+		if (authUser) {
+			var ref = new Firebase("https://beer-sol-kanar.firebaseio.com/users/" + authUser.uid);
+			var sync = $firebase(ref);
+			return sync.$asObject();
+		}
+		return null;
+	}
+
+	return {
+		user: getUser(),
+		getUser: getUser
+	};
 }]);
+app.run(function ($rootScope, UserProvider) {
+	if (UserProvider.user) {
+		UserProvider.user.$bindTo($rootScope, "user");
+	}
+});
 app.controller("calculatorController", ["$scope", "$firebase","Auth", function($scope, $firebase, Auth){
 
 	$scope.User = Auth.$getAuth();
@@ -79,71 +92,69 @@ app.controller('listController', ["$scope", "$firebase", "Auth", function($scope
 	$scope.reverse = false;
 
 	$scope.order = function (sortValue) {
-	    if (sortValue == $scope.sortValue) {
-	        $scope.reverse = !$scope.reverse;
-	    }
-	    $scope.sortValue = sortValue;
+		if (sortValue == $scope.sortValue) {
+			$scope.reverse = !$scope.reverse;
+		}
+		$scope.sortValue = sortValue;
 	}
 
-    $scope.showEdit = (Auth.$getAuth()) ? true: false;
-    $scope.beers = sync.$asArray();
+	$scope.showEdit = (Auth.$getAuth()) ? true: false;
+	$scope.beers = sync.$asArray();
 
-    $scope.getBeerValue = function (beer) {
-        var value = (beer.percentAlcohol * beer.ounces) / beer.price;
-        return Math.round(value * 10) / 10;
-    };
+	$scope.getBeerValue = function (beer) {
+		var value = (beer.percentAlcohol * beer.ounces) / beer.price;
+		return Math.round(value * 10) / 10;
+	};
 }])
 
 app.controller('editController', ['$scope', "$firebase", '$routeParams', '$location', function ($scope, $firebase, $routeParams, $location) {
-    var ref = new Firebase("https://beer-sol-kanar.firebaseio.com/beers/" + $routeParams.id);
-    var sync = $firebase(ref);
+	var ref = new Firebase("https://beer-sol-kanar.firebaseio.com/beers/" + $routeParams.id);
+	var sync = $firebase(ref);
 
-    var syncObject = sync.$asObject();
-    syncObject.$bindTo($scope, "beer");
+	var syncObject = sync.$asObject();
+	syncObject.$bindTo($scope, "beer");
 
-    $scope.delete = function () {
-        sync.$remove();
-        $location.path("/list");
-    };
+	$scope.delete = function () {
+		sync.$remove();
+		$location.path("/list");
+	};
 }]);
-app.controller('loginController', ["$scope", "Auth", function ($scope, Auth) {
+app.controller('loginController', ["$scope", "Auth", '$rootScope', 'UserProvider', function ($scope, Auth, $rootScope, UserProvider) {
 
-    if (Auth.$getAuth()) {
-        Auth.$unauth();
-    }
+	if (Auth.$getAuth()) {
+		Auth.$unauth();
+		$rootScope.user = null;
+	}
 
-    $scope.CreateNewUser = function () {
-        $scope.message = null;
-        $scope.error = null;
+	$scope.CreateNewUser = function () {
+		$scope.message = null;
+		$scope.error = null;
 
-        Auth.$createUser({
-            email: $scope.email,
-            password: $scope.password
-        }).then(function (userData) {
-            $scope.message = "User created with uid: " + userData.uid;
-        }).catch(function (error) {
-            $scope.error = error;
-        });
-    };
+		Auth.$createUser({
+			email: $scope.email,
+			password: $scope.password
+		}).then(function (userData) {
+			$scope.message = "User created with uid: " + userData.uid;
+		}).catch(function (error) {
+			$scope.error = error;
+		});
+	};
 
-    $scope.Login = function () {
-        $scope.message = null;
-        $scope.error = null;
+	$scope.Login = function () {
+		$scope.message = null;
+		$scope.error = null;
 
-        Auth.$authWithPassword({
-            email: $scope.email,
-            password: $scope.password
-        }).then(function (authData) {
-            $scope.message = "Logged in as:" + authData.uid;
-        }).catch(function (error) {
-            $scope.error = error;
-        });
-    }
+		Auth.$authWithPassword({
+			email: $scope.email,
+			password: $scope.password
+		}).then(function (authData) {
+			$scope.showLoggedIn = true;
+			UserProvider.getUser().$bindTo($rootScope, "user");
+		}).catch(function (error) {
+			$scope.error = error;
+		});
+	}
 }]);
-app.controller('profileController', ['$scope', 'User', '$firebase','Auth', function ($scope, User, $firebase, Auth) {
-    if (User) {
-        User.$bindTo($scope, "userProfile");
-    } else {
-        $scope.message = "Login first to access your profile";
-    }
+app.controller('profileController', ['$scope', '$rootScope', function ($scope, $rootScope) {
+
 }]);
